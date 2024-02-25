16281554c479923722ad7bb1adf9e164c6f20546
Viết Minh Hiếu <hieuvm@omegatheme.com>
2024-02-25 16:44:08 +0700

    feat: finish section 6

---
+++ src/disk/disk.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { DiskService } from './disk.service';
+import { PowerModule } from 'src/power/power.module';
+
+@Module({
+  imports: [PowerModule],
+  exports: [DiskService],
+  providers: [DiskService],
+})
+export class DiskModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { NestFactory } from '@nestjs/core';
-import { MessagesModule } from './messages/messages.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ComputerModule } from './computer/computer.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(MessagesModule);
+  const app = await NestFactory.create(ComputerModule);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(4000);
 }
